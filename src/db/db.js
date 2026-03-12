515321fc912795447ccc84718034aa5b3cbebc5c
vishwamdhavale <vishwamdhavale@gmail.com>
2026-03-12 22:41:46 +0530

    fix

--- src/db/db.js
+++ src/db/db.js
@@ -3,9 +3,11 @@ import { DB_Name } from "../constants.js";
 
 const connectdb = async () => {
     try {
-        const maskedUri = process.env.MONGO_URI ? process.env.MONGO_URI.replace(/\/\/[^:]+:[^@]+@/, "//***:***@") : "MISSING";
+        const baseUri = process.env.MONGO_URI ? process.env.MONGO_URI.replace(/\/+$/, "") : "";
+        const maskedUri = baseUri.replace(/\/\/[^:]+:[^@]+@/, "//***:***@");
         console.log(`Connecting to MongoDB: ${maskedUri}/${DB_Name}`);
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_Name}`)
+        
+        const connectionInstance = await mongoose.connect(`${baseUri}/${DB_Name}`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
         
         mongoose.connection.on('error', (err) => {
